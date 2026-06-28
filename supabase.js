33fbc2ecb34c0df82fe8369c7e2fb44077442f8a
escobaraldo163-collab <escobaraldo163@gmail.com>
2026-06-28 01:44:38 -0600

    Usar anon public key

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://cvauyuqlxclpzifbytuw.supabase.co";
-const SUPABASE_KEY = "sb_publishable_JVpfQx90zg-GmeSr-NeDyA_QM0tEa4i";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2YXV5dXFseGNscHppZmJ5dHV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2MTExMTIsImV4cCI6MjA5ODE4NzExMn0.l5wpHDS19WgeEQtYyM7w4GpnPRUYnViOjEgj7I_TEE4";
 
 window.SUPABASE_CONFIG = {
   url: SUPABASE_URL,
